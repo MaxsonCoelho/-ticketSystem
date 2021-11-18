f5981961d3ff4cc3e1bbced67962ffbf19deb67a
Maxson <maxsoncoelho@gmail.com>
2021-11-18 14:38:52 -0400

    create page register

--- ticketsystem/src/pages/SignUp/index.js
+++ ticketsystem/src/pages/SignUp/index.js
@@ -1,7 +1,35 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './signup.css';
+import logo from '../../assets/logo.png';
+
 export default function SignUp() {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit(e){
+    e.preventDefault();
+    alert('funcionando!');
+  }
+
     return (
-      <div>
-        <h1>Cadastro</h1>
+      <div className="container-center">
+        <div className="login">
+          <div className="login-area">
+            <img src={logo} alt="Sistema Logo" />
+          </div>
+
+          <form onSubmit={handleSubmit}>
+            <h1>Cadastrar</h1>
+            <input type="text" placeholder="nome completo" value={nome} onChange={e=> setNome(e.target.value)}/>
+            <input type="text" placeholder="email@email.com" value={email} onChange={e=> setEmail(e.target.value)}/>
+            <input type="password" placeholder="*********" value={password} onChange={e=> setPassword(e.target.value)}/>
+            <button type="submit">Criar conta</button>
+          </form>
+
+          <Link to="/register">Já tenho uma conta</Link>
+        </div>
       </div>
     );
   }
