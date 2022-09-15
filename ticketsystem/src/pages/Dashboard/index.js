994b9d23325af028dee312725634eded3072a4ba
max <masoncoelho@gmail.com>
2022-09-15 17:42:59 -0400

    complete signOut

--- ticketsystem/src/pages/Dashboard/index.js
+++ ticketsystem/src/pages/Dashboard/index.js
@@ -1,9 +1,15 @@
+import { useContext } from "react";
+import { AuthContext } from "../../context/auth";
+
 export default function Dashboard() {
-    return (
-      <div>
-        <h1>Dashboard</h1>
-      </div>
-    );
-  }
+  const { signOut } = useContext(AuthContext);
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <button onClick={() => signOut()}>Fazer Logout</button>
+    </div>
+  );
+}
   
  
